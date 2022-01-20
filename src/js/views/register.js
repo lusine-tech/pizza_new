5707e1c5763a73a4b3750dbffdf2bb1d0fe43366
Seryozha02 <seryozha.asatryan02@gmail.com>
2022-01-20 23:52:59 +0400

    ProductMenu

--- src/js/views/register.js
+++ src/js/views/register.js
@@ -1,11 +1,22 @@
 const renderRegisterPage = () => {
-    const container = `<select name="tables" id="selectTable" class="table-name">
-      <option value="Սեղանի համար" class="tableNumber"selected>Սեղանի համար</option>  
-      <option value="1">1</option>  
-      <option value="2">2</option>
-      <option value="3">3</option>
-    </select> 
-    <button class="comfirm-btn">Ամրագրել</button> `;
+    const container = `  <div class="select">
+    <form action="#" name="filter">
+      <select name="table-number" id="select_table">
+        <option value="#" readonly>
+          Սեղանի համարը
+          <div class="triangle"></div>
+        </option>
+        <option value="1">սեղան 1</option>
+        <option value="2">սեղան 2</option>
+        <option value="3">սեղան 3</option>
+        <option value="4">սեղան 4</option>
+        <option value="5">սեղան 5</option>
+        <option value="4">սեղան 6</option>
+        <option value="5">սեղան 7</option>
+      </select>
+      <input type="submit" class="book" value="Ամրագրել" />
+    </form>
+  </div>`;
   
     document.querySelector(".container").innerHTML = container;
   };
