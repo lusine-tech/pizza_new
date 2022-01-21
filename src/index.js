0c149fe30bb8adb59267e31560bd08b5c987c2eb
lusine-tech <lusinee@yahoo.com>
2022-01-21 23:25:17 +0400

    page on click

--- src/index.js
+++ src/index.js
@@ -6,7 +6,22 @@ import { renderZambyuxPage } from "./js/views/zumbyux";
 import router from "./js/routing";
 
 renderRegisterPage();
-let book = document.querySelector(".book");
-book.addEventListener("click", renderChoicePage);
+const table = document.querySelector("#select_table");
+const book = document.querySelector(".book");
+
+
 //renderChoicePage();
 //router.redirect("/menu");
+book.onclick = (e) => {
+
+  e.preventDefault();
+  
+  const selectedValues = [].filter
+    .call(table.options, (option) => option.selected)
+    .map((option) => option.text);
+   
+  renderChoicePage();
+   document.body.style.backgroundImage = "url('../image/ej2.jpg')";
+
+  //alert(selectedValues);
+};
--- src/js/views/register.js
+++ src/js/views/register.js
@@ -1,10 +1,10 @@
 const renderRegisterPage = () => {
     const container = `  <div class="select">
-    <form action="#" name="filter">
+    
       <select name="table-number" id="select_table">
         <option value="#" readonly>
           Սեղանի համարը
-          <div class="triangle"></div>
+          
         </option>
         <option id = "table-1" value="1">սեղան 1</option>
         <option id = "table-2" value="2">սեղան 2</option>
@@ -14,8 +14,8 @@ const renderRegisterPage = () => {
         <option id = "table-6" value="6">սեղան 6</option>
         <option id = "table-7" value="7">սեղան 7</option>
       </select>
-      <input type="submit" class="book" value="Ամրագրել" />
-    </form>
+      <button class="book"> Ամրագրել<button />
+    
   </div>`;
   
     document.querySelector(".container").innerHTML = container;
