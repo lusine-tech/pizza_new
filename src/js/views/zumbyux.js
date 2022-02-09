867846c29b2f6ea728f220a4367d1ee8cd83cf2b
Sgalstyan06 <96201224+Sgalstyan06@users.noreply.github.com>
2022-02-09 11:42:58 +0400

    zambyux

--- src/js/views/zumbyux.js
+++ src/js/views/zumbyux.js
@@ -1,10 +1,9 @@
 import { State } from "../model";
-import {confirmOrder} from "../helpers/eventListeners"
+import { confirmOrder } from "../helpers/eventListeners";
 import { headerMenuEventListeners } from "../helpers/eventListeners";
 
-
 const renderZambyuxPage = () => {
-console.log(State.basket);
+  console.log(State.basket);
 
   const container = `<div class="body">
    
@@ -26,8 +25,9 @@ console.log(State.basket);
       </footer>
     </main>
   </div>`;
-let orders = State.basket.reduce((acc, elem) => {
-acc += `<section>
+  let idUniq = 0;
+  let orders = State.basket.reduce((acc, elem) => {
+    acc += `<section id = "${elem.id}">
         <div class="img">
           <img src="./image/pizzaImg/pizza2.png" alt="" />
         </div>
@@ -35,18 +35,65 @@ acc += `<section>
         <div>Գին։ ${elem.price} ${elem.currency} </div>
         <div>   ${elem.quantity} </div>
         <div>Արժեք։ ${elem.price * elem.quantity} ${elem.currency}</div>
-        <div>
-          <img
+        <div class = "cancelOrder">
+          <img 
             src="./image/icons/close/1x/outline_close_white_18dp.png"
             alt=""
           />
         </div>
+        
       </section>`;
-      return acc
-},"")
-  //document.querySelector(".suma") 
-document.querySelector(".container").innerHTML = container;
-document.querySelector(".basket_block").insertAdjacentHTML("afterbegin", orders);
+    return acc;
+  }, "");
+  //document.querySelector(".suma")
+
+  document.querySelector(".container").innerHTML = container;
+  document
+    .querySelector(".basket_block")
+    .insertAdjacentHTML("afterbegin", orders);
+
+  document.querySelectorAll(".cancelOrder").forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      console.log(this);
+      console.log(this.closest("section"));
+      console.log(this.closest("section").id);
+      let closeId = this.closest("section").id;
+      let removedElem = State.basket.find(function (elem) {
+        return elem.id === +closeId;
+      });
+      let filterBasket = State.basket.filter(function (elem) {
+        return elem.id !== +closeId;
+      });
+      debugger;
+      let sections = document.querySelectorAll(".basket_block section");
+      for (let i = 0; i < sections.length; i++) {
+        document.querySelector(".basket_block").removeChild(sections[i]);
+      }
+      State.basket = filterBasket;
+      
+      console.log(filterBasket);
+      let newOrders = State.basket.reduce((acc, elem) => {
+        acc += `<section id = "${elem.id}">
+            <div class="img">
+              <img src="./image/pizzaImg/pizza2.png" alt="" />
+            </div>
+            <div>${elem.name}</div>
+            <div>Գին։ ${elem.price} ${elem.currency} </div>
+            <div>   ${elem.quantity} </div>
+            <div>Արժեք։ ${elem.price * elem.quantity} ${elem.currency}</div>
+            <div class = "cancelOrder">
+              <img 
+                src="./image/icons/close/1x/outline_close_white_18dp.png"
+                alt=""
+              />
+            </div>
+            
+          </section>`;
+        return acc;
+      }, "");
+      document.querySelector('.basket_block').insertAdjacentHTML("afterbegin", newOrders);
+    });
+  });
 };
 
 export { renderZambyuxPage };
